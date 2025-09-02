699026954334281325f91cab6fadeff421787c8e
gabriel gama <bielmgama@gmail.com>
2025-09-01 23:51:20 -0300

    feat(login): adiciona dependencia de componentes

--- frontend/my-movie/app/_layout.tsx
+++ frontend/my-movie/app/_layout.tsx
@@ -1,14 +1,17 @@
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
-import { ptBR } from '@clerk/localizations';
-import { Slot } from "expo-router";
-import "../styles.css";
+import '../global.css';
+
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
-    
   return (
-    <ClerkProvider localization={ptBR} tokenCache={tokenCache}>
-      <Slot />
+    <ClerkProvider tokenCache={tokenCache}>
+      <GluestackUIProvider mode="dark">
+        <Slot/>
+      </GluestackUIProvider>
     </ClerkProvider>
-  )
+  );
 }
+
