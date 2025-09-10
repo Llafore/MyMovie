eb3c890ac146b8b690c6b8724eb73e5720e3131f
gabriel <bielmgama@gmail.com>
2025-09-10 12:40:59 -0300

    feat(login): altera estilo do botao de sign out

--- frontend/my-movie/app/components/SignOutButton.tsx
+++ frontend/my-movie/app/components/SignOutButton.tsx
@@ -1,13 +1,19 @@
+import { AlertDialog, AlertDialogBackdrop, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader } from '@/components/ui/alert-dialog'
+import { Button, ButtonText } from '@/components/ui/button'
 import { useClerk } from '@clerk/clerk-expo'
-import * as Linking from 'expo-linking'
 import { useRouter } from 'expo-router'
-import { Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Text } from 'react-native'
 
-export const SignOutButton = () => {
+const SignOutButton = () => {
   // Use `useClerk()` to access the `signOut()` function
   const { signOut } = useClerk()
   const router = useRouter()
-  
+
+  const [mostrarDialogConfirmacao, setMostrarDialogConfirmacao] = React.useState(false);
+
+  const handleClose = () => setMostrarDialogConfirmacao(false);
+
   const handleSignOut = async () => {
     try {
       await signOut()
@@ -19,10 +25,54 @@ export const SignOutButton = () => {
       console.error(JSON.stringify(err, null, 2))
     }
   }
+
+  const DialogConfirmarSair = () => {
+    return (
+      <AlertDialog isOpen={mostrarDialogConfirmacao} onClose={handleClose} size="md">
+        <AlertDialogBackdrop />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Text className="text-white font-bold text-lg py-2">
+              Sair
+            </Text>
+          </AlertDialogHeader>
+
+          <AlertDialogBody className="py-2">
+            <Text className="text-white">
+              Tem certeza que deseja sair?
+            </Text>
+          </AlertDialogBody>
+
+          <AlertDialogFooter className="py-2">
+            <Button
+              variant="outline"
+              action="secondary"
+              onPress={handleClose}
+              size="md"
+            >
+              <ButtonText>Cancelar</ButtonText>
+            </Button>
+
+            <Button size="md" onPress={handleSignOut} className="bg-red-600 data-[active=true]:bg-red-800">
+              <ButtonText>Sair</ButtonText>
+            </Button>
+          </AlertDialogFooter>
+
+        </AlertDialogContent>
+      </AlertDialog>
+    )
+  }
+
   return (
-    <TouchableOpacity onPress={handleSignOut} className='px-4 py-2 bg-red-600 rounded-full'>
-      <Text className='text-white'>Sair</Text>
-    </TouchableOpacity>
+    <>
+      <Button variant='outlined' size='xl' className='w-full bg-transparent rounded-full border border-red-500 transition data-[active=true]:bg-neutral-700 disabled:bg-primary-black' onPress={() => setMostrarDialogConfirmacao(true)}>
+        <ButtonText className='text-white'>Sair</ButtonText>
+      </Button>
+      <DialogConfirmarSair />
+    </>
+
+
   )
 }
 
