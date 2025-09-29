6d2b8513f6890a9b67c9b2b50232886007b919dc
gabriel <bielmgama@gmail.com>
2025-09-28 23:07:42 -0300

    feat(home): altera porta padrao

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from api.routes import user_api, media_api
 
-import uvicorn
-
 app = FastAPI()
 
 app.include_router(user_api.router)
 app.include_router(media_api.router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    import os
+    from dotenv import load_dotenv
+    
+    load_dotenv()
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
