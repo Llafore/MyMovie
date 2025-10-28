a1ccf64865f27b8e9a5089cecd2013173f7e3c91
gabriel <bielmgama@gmail.com>
2025-10-28 20:16:39 -0300

    feat(recommendations): adiciona generos ao retorno do endpoint de recomendacoes

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -36,7 +36,7 @@ def search_genres():
 def search_movies(genres: list[dict[int | str]] = None):
     print("Fetching movies...")
     dao = MediaDAO()
-    movies = (fetch_top_rated_movies(pages=50) + fetch_popular_movies(pages=50) + fetch_movies_by_genres(genres, pages_by_genre=5))
+    movies = (fetch_top_rated_movies(pages=10) + fetch_popular_movies(pages=10) + fetch_movies_by_genres(genres, pages_by_genre=2))
 
     normalized_movies, normalized_movies_genres = normalize_media_data(movies, is_movie=True)
 
@@ -48,7 +48,7 @@ def search_movies(genres: list[dict[int | str]] = None):
 def search_series(genres: list[dict[int | str]] = None):
     print("Fetching series...")
     dao = MediaDAO()
-    series = (fetch_top_rated_series(pages=50) + fetch_popular_series(pages=50) + fetch_series_by_genres(genres, pages_by_genre=5))
+    series = (fetch_top_rated_series(pages=10) + fetch_popular_series(pages=10) + fetch_series_by_genres(genres, pages_by_genre=2))
 
     normalized_series, normalized_series_genres = normalize_media_data(series, is_movie=False)
 
